b37cb31270ebc7e6e3325c3260207b8e0547c489
João Vitor da Silva <jaumm.silva.04@gmail.com>
2023-01-04 19:21:44 -0300

    Aula 21

---
+++ ConsoleApp3/Classes/Email.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Classes
+{
+    public class Email
+    {
+        private  Email() { }
+
+        private static Email instancia;
+        public  string Origin;
+        public  string Destino;
+        public  string Titulo;
+        public  string Corpo;
+
+        public void EnviarEmail()
+        {
+            Console.WriteLine("Enviando Email para " + Destino + "\nCom o titulo: "
+                + Titulo + "\nCorpo " + Corpo);
+        }
+
+        public static Email Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Email();
+                }
+                return instancia;
+            }
+        }
+    }
+}
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,24 +11,16 @@ namespace ConsoleApp3
     class Program
     {
         public static void Main(string[] args)
-        {     
+        {
+            Email.Instancia.Corpo = "bla bla bla";
+            Email.Instancia.Titulo = "Titulo do Email";
+            Email.Instancia.Destino = "Destinodo@Email.com";
+            Email.Instancia.Origin = "Origindo@Email.com";
+
+            Email.Instancia.EnviarEmail();
+            
 
             ProgOrientadaFuncao.FunçaoOrientada();                 
         }
-        partial class Animal
-        {
-            public string teste;
-            partial void tt();
-        }
-        partial class Animal
-        {
-            public string teste2;
-            partial void tt()
-            {
-                Console.WriteLine("teste");
-            }
-        }
-
-
     }
 }
