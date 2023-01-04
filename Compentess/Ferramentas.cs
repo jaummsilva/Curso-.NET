6128ce5dd32d721f18cb397c3dd48700e67b8b1b
João Vitor da Silva <jaumm.silva.04@gmail.com>
2023-01-03 21:55:22 -0300

    aula 16

---
+++ Compentess/Ferramentas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compentess
+{
+    public class Ferramentas
+    {
+        internal string MetodoSomenteParaMeuAssembly()
+        {
+            return "Este método só pode ser acessado dentro deste assembly componentess";
+        }
+        public string MetodoParaTodosQueUtilizarMeuAssembly()
+        {
+            
+            return "Este método é para todos";
+        }
+    }
+}
---
+++ Componente/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Componente
+{
+    public class Class1
+    {
+    }
+}
